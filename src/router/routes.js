d27a89933cc29ca926c7b5b015e2bad7b2dc1385
Ryutaro Kawakami <ne200152g@gmail.com>
2019-01-08 15:49:01 +0900

    ルーティング追加

---
+++ src/router/routes.js
@@ -0,0 +1,25 @@
+import KbnBoardView from '@/components/templates/KbnBoardView.vue'
+import KbnLoginView from '@/components/templates/KbnLoginView.vue'
+import KbnTaskDetailModal from '@/components/templates/KbnTaskDetailModal.vue'
+
+export default [
+  {
+    path: '/',
+    component: KbnBoardView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/login',
+    component: KbnLoginView,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '/tasks/:id',
+    component: KbnTaskDetailModal,
+    meta: { requiresAuth: true }
+  },
+  {
+    path: '*',
+    redirect: '/'
+  }
+]
