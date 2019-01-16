a9c796756efdbec004d5bfa49ecd5d59e28701fb
Ryutaro Kawakami <ne200152g@gmail.com>
2019-01-16 18:49:52 +0900

    eslintとprettierを導入

--- src/components/atoms/KbnButton.vue
+++ src/components/atoms/KbnButton.vue
@@ -1,4 +1,11 @@
 <template>
-  <button :class="classes" 
-:disabled="disabled" type="button"/>
+  <button :class="classes" :disabled="disabled" type="button"/>
 </template>
+
+<script>
+const a = 'a';
+alert(a);
+// export default {
+
+// }
+</script>
